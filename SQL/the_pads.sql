bf37eef65b19443d6ae31385926b7d4c66aaae0a
Farahani Samat <farahanis@gmail.com>
2019-07-26 21:28:23 +1000

    Rearrange directory for SQL problems.

---
+++ SQL/the_pads.sql
@@ -0,0 +1,5 @@
+select (Name||'('||substr(Occupation,1,1)||')') 
+from OCCUPATIONS order by Name;
+
+select ('There are a total of '||count(Occupation)||' '||lower(Occupation)||'s.') 
+from OCCUPATIONS group by Occupation order by count(Occupation), Occupation;
