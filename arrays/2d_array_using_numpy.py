4e7744f0e1907b37f87ba95222ea31ce86696515
Farahani Samat <farahanis@gmail.com>
2019-07-27 11:44:39 +1000

    2d array challenge solution using numpy

---
+++ arrays/2d_array_using_numpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.random.randint(-9, 9, (6, 6))
+
+
+def hourglassSum(arr):
+    hourglasslist = []
+    for i in range(4):
+        for j in range(4):
+            sumhourglass = np.sum(arr[i][j:j + 3]) + arr[i + 1][j + 1] + np.sum(arr[i + 2][j:j + 3])
+            hourglasslist.append(sumhourglass)
+    return max(hourglasslist)
+
+print(arr)
+print(hourglassSum(arr))
